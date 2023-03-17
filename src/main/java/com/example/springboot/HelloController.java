1b38f747a87602c7f7516fd063e4cec0e3beb253
tfynes-pivotal <tfynes@pivotal.io>
2023-03-17 09:08:28 -0400

    Update HelloController.java

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -8,6 +8,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
+		System.out.println("Greetings from Spring Boot + Tanzu");
 		return "Greetings from Spring Boot + Tanzu!";
 	}
 
